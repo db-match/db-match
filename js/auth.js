aa10385993e9357f263313b8ea14a2fe663e614d
db-match <zinirampura@gmail.com>
2026-05-11 23:58:02 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,19 +1,60 @@
-console.log("Authentication System Ready")
+const signupForm=document.querySelector("form");
 
-function loginUser(){
+if(signupForm){
 
-console.log("Login function initialized")
+signupForm.addEventListener("submit",async(e)=>{
+
+e.preventDefault();
+
+const inputs=signupForm.querySelectorAll("input");
+
+const fullName=inputs[0].value;
+
+const email=inputs[1].value;
+
+const phone=inputs[2].value;
+
+const password=inputs[3].value;
+
+try{
+
+const userCredential=
+
+await auth.createUserWithEmailAndPassword(
+email,
+password
+);
+
+const user=userCredential.user;
+
+await db.collection("users").doc(user.uid).set({
+
+uid:user.uid,
+
+name:fullName,
+
+email:email,
+
+phone:phone,
+
+approved:false,
+
+verified:false,
+
+createdAt:new Date()
+
+});
+
+alert("Account Created Successfully");
+
+window.location.href="discover.html";
+
+}catch(error){
+
+alert(error.message);
 
 }
 
-function signupUser(){
-
-console.log("Signup function initialized")
-
-}
-
-function logoutUser(){
-
-console.log("Logout function initialized")
+});
 
 }
