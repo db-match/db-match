4a8a1fd0aaec20f6e36ed4839a7f51e8fc9740a2
db-match <zinirampura@gmail.com>
2026-05-11 23:10:54 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,19 @@
+console.log("Authentication System Ready")
+
+function loginUser(){
+
+console.log("Login function initialized")
+
+}
+
+function signupUser(){
+
+console.log("Signup function initialized")
+
+}
+
+function logoutUser(){
+
+console.log("Logout function initialized")
+
+}
