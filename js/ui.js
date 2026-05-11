7351a0f67607aff063ffeae6792c394019763fd4
db-match <zinirampura@gmail.com>
2026-05-11 23:11:30 +0530

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,19 @@
+console.log("UI Components Ready")
+
+function showToast(message){
+
+alert(message)
+
+}
+
+function showLoader(){
+
+console.log("Loader started")
+
+}
+
+function hideLoader(){
+
+console.log("Loader stopped")
+
+}
